7bc915c5e5cafc34087e37b8c26e352df62a7ff9
Maritza Ulfa Huriyah <maritza.ulfa.huriyah@gmail.com>
2024-09-18 09:32:38 +0700

    Pertemuan 4 - Praktikum 2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -36,9 +36,7 @@ class UserController extends Controller
         // UserModel::where('username', 'customer-1')->update($data); // update data user        
 
         // coba akses model UserModel
-        $user = UserModel::findOr(20, ['username', 'nama'], function(){
-            abort(404);
-        }); // ambil semua data dari tabel m_user
+        $user = UserModel::findOrFail(1); // ambil semua data dari tabel m_user
         return view('Pertemuan3.user', ['data' => $user]);
     }
 }
