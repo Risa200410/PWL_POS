0ea137a3e2d50e14fa79b4ec63fc225aae349547
Maritza Ulfa Huriyah <maritza.ulfa.huriyah@gmail.com>
2024-10-01 20:56:01 +0700

    Pertemuan 5 - Tugas

--- app/Models/LevelModel.php
+++ app/Models/LevelModel.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class LevelModel extends Model
 {
@@ -20,4 +21,10 @@ class LevelModel extends Model
      * @var array
      */
     protected $fillable = ['level_kode', 'level_nama'];
+
+    // Relasi dengan UserModel
+    public function users(): HasMany
+    {
+        return $this->hasMany(UserModel::class, 'level_id', 'level_id');
+    }
 }
