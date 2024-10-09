e9551946856f3fb2ea35afe84cb6e0d973eac64d
Maritza Ulfa Huriyah <maritza.ulfa.huriyah@gmail.com>
2024-10-09 10:16:50 +0700

    Pertemuan 7 - Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,8 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Model
+// class UserModel extends Model
+// {
+//     use HasFactory;
+
+//     protected $table = 'm_user'; // mendefinisikan nama tabel yang digunakan oleh model ini
+//     protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel yang digunakan
+//     /**
+//      * The attributes that are mass assignable.
+//      * 
+//      * @var array
+//      */
+//     // protected $fillable = ['level_id', 'username', 'nama', 'password'];
+//     protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
+
+//     protected $hidden = ['password'];
+//     protected $casts = ['password' => 'hashed'];
+
+//     public function level(): BelongsTo {
+//         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+//     }
+// };
+
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
@@ -17,7 +40,11 @@ class UserModel extends Model
      * 
      * @var array
      */
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    // protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
+
+    protected $hidden = ['password'];
+    protected $casts = ['password' => 'hashed'];
 
     public function level(): BelongsTo {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\LevelController;
 use Illuminate\Http\Request;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\BarangController;
+use App\Http\Controllers\AuthController;
 
 
 
@@ -176,3 +177,15 @@ Route::group(['prefix' => 'barang'], function () {
     Route::put('{id}', [BarangController::class, 'update']); //menyimpan perubahan data user
     Route::delete('{id}', [BarangController::class, 'destroy']); //menghapus data user
 });
+
+Route:: pattern ('id','[0-9]+'); // artinya ketika ada parameter {id}, maka harus berupa angka
+
+Route:: get ('login', [AuthController:: class, 'login' ])->name('login' );
+Route:: post ('login', [AuthController :: class, 'postlogin' ]);
+Route:: get('logout', [AuthController::class, 'logout' ])->middleware('auth' );
+
+Route::middleware(['auth' ])->group(function(){ // artinya semua route di dalam group ini harus login dulu
+
+// masukkan semua route yang perlu autentikasi di sini
+});
+
